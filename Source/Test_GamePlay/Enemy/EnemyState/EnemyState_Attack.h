95ff931907b96f9094e0ad982ca96d93be7ba1f4
Nayuta <2804737972@qq.com>
2026-07-10 21:58:36 +0800

    敌人类框架

---
+++ Source/Test_GamePlay/Enemy/EnemyState/EnemyState_Attack.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Common/StateBase.h"
+#include "EnemyState_Attack.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TEST_GAMEPLAY_API UEnemyState_Attack : public UStateBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/Test_GamePlay/Enemy/EnemyState/EnemyState_Idle.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Common/StateBase.h"
+#include "EnemyState_Idle.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TEST_GAMEPLAY_API UEnemyState_Idle : public UStateBase
+{
+	GENERATED_BODY()
+	
+};
